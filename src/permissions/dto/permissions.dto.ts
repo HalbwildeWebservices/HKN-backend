b0a673e0419361ed0be8ef15a44dc8edee7f78dd
HKN - Ralf Krauth <website@halbwilde.com>
2022-11-13 02:34:45 +0100

    improve permission dto / swagger-ui

--- src/permissions/dto/permissions.dto.ts
+++ src/permissions/dto/permissions.dto.ts
@@ -1,9 +1,16 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsArray } from "class-validator";
+import { IsNotEmpty, IsArray, IsEnum,  } from "class-validator";
+import { EPermission } from "hkn-common";
 
 export class SetPermissionsDto {
-    @ApiProperty({example: '["read_user"]', description: "permission list", type: [String]})
+    @ApiProperty({
+        example: [EPermission.DEFAULT, EPermission.ADD_USER], 
+        description: "permission list", 
+        enum: EPermission,
+        isArray: true,
+    })
     @IsNotEmpty()
     @IsArray()
+    @IsEnum(EPermission, {each: true})
     permissions: string[]
 }
